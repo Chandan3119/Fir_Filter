1889c37b5b95d0919f39a58280abf8f609c170ff
Chandan3119 <105112485+Chandan3119@users.noreply.github.com>
2023-02-06 15:30:58 +0530

    Update Fir_Filter_neon.c

--- Fir_Filter_neon.c
+++ Fir_Filter_neon.c
@@ -1,8 +1,6 @@
 #include<arm_neon.h>
 #include<stdio.h>
 
-
-
 void fir_reset(FIR_T *fir, UINT32 n_taps, const COMPLEX_R64 coeff[])
 {
     UINT32 i = 0;
@@ -24,88 +22,60 @@ void fir_reset(FIR_T *fir, UINT32 n_taps, const COMPLEX_R64 coeff[])
     }
 }
 
-
 void fir_filter(FIR_T *fir, COMPLEX_R64 *x, UINT32 n, COMPLEX_R64 *y)
 {
     UINT32 com_idx = fir->fir_com_idx;
     UINT32 n_taps = fir->fir_n_taps;
-   // COMPLEX_R64 *h = fir->fir_h;
-
     UINT32 i, j,k;
-   // memset(k, 0, sizeof(n_taps));
-   
     float64_t m11[2],m21,m13[2],m12;
     float64x2_t m31,m41;    
     float64x2_t x_real, x_imag, h_real, h_imag;
-    
     float64x2x2_t h_val,x_val;
-     double zero[] = {0.0};
+    double zero[] = {0.0};
     x_val = vld2q_dup_f64(zero);  h_val = vld2q_dup_f64(zero);
     COMPLEX_R64 *h = fir->fir_h,x_v[n_taps];
-     
-        
+            
     for (j = 0; j < n; j++)
     {
         fir->fir_state[com_idx] = x[j];
         fir->fir_state[com_idx + n_taps] = x[j];
-        
-         float64x2_t vaccum_real = vdupq_n_f64(0);
-         float64x2_t vaccum_imag = vdupq_n_f64(0);
+        float64x2_t vaccum_real = vdupq_n_f64(0);
+        float64x2_t vaccum_imag = vdupq_n_f64(0);
         
         for (i = 0; i < n_taps; i++)
         {
             k = (n_taps + com_idx - i);        
             x_v[i] = fir->fir_state[k];                 
         }
-        
-       
         // vectorize the inner loop
         for (i = 0; i < n_taps; i+=2)
         {   
-            
-          
-           
             //interleaved load of x coefficients
             x_val = vld2q_f64(x_v + i);    
             x_real = x_val.val[0];          
             x_imag = x_val.val[1];          
-            
             // interleaved load of filter coefficients
             h_val = vld2q_f64(h + i);
             h_real = h_val.val[0];         
             h_imag = h_val.val[1];         
-            
-             //Multiply (a+ib)(x+iy)=(ax-by)+i(ay+bx)
-
+            //Multiply (a+ib)(x+iy)=(ax-by)+i(ay+bx)
             vaccum_real = vmlaq_f64(vaccum_real, h_real, x_real);     
             vaccum_real = vmlsq_f64(vaccum_real, h_imag, x_imag);  
             vaccum_imag = vmlaq_f64(vaccum_imag, h_real, x_imag);   
             vaccum_imag = vmlaq_f64(vaccum_imag, h_imag, x_real);
-           
             vst1q_f64(m11,vaccum_real); m31=vld1q_f64(m11); m21=vaddvq_f64(m31);   
-            vst1q_f64(m13,vaccum_imag); m41=vld1q_f64(m13); m12=vaddvq_f64(m41);   
-        
-           
+            vst1q_f64(m13,vaccum_imag); m41=vld1q_f64(m13); m12=vaddvq_f64(m41);    
         }
             com_idx = com_idx + 1;
             if (com_idx == n_taps)
             {
                 com_idx = 0;
             }
-            
-            // com_idx = ( com_idx + 1 ) % n_taps;
             y[j].real=(double)m21;
             y[j].imag=(double)m12;
-            
-            
-            
-           
-              
-           
-            
+                       
    }
         
         fir->fir_com_idx = com_idx;
-       
-       
+           
 }
